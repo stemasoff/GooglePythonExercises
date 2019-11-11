32465ca54c67202019924e347ed856f89d91ab8b
Ilya <stemasoff1@yandex.ru>
2019-11-11 12:15:35 +0300

    Доделал BabyNameExercise

--- BabyNamesExercise/babynames.py
+++ BabyNamesExercise/babynames.py
@@ -2,7 +2,7 @@ import re
 import sys
 
 """Baby Names exercise
-
+Part A
 Define the extract_names() function below and change main()
 to call it.
 
@@ -24,6 +24,20 @@ Suggested milestones for incremental development:
  -Get the names data into a dict and print it
  -Build the [year, 'name rank', ... ] list and print it
  -Fix main() to use the extract_names list
+
+Part B
+Suppose instead of printing the text to standard out, we want to write files containing the text.
+If the flag --summaryfile is present, do the following:
+for each input file 'foo.html',
+instead of printing to standard output, write a new file 'foo.html.summary' 
+that contains the summary text for that file.
+
+Once the --summaryfile feature is working, run the program on all the 
+files using * like this: "./babynames.py --summaryfile baby*.html".
+This generates all the summaries in one step.
+(The standard behavior of the shell is that it expands the "baby*.html"
+pattern into the list of matching filenames, and then the shell runs babynames.py,
+passing in all those filenames in the sys.argv list.)
 """
 
 
@@ -35,7 +49,7 @@ def extract_names(filename):
 
     year = re.search(r'Popularity\sin\s(\d{4})', text)
     names.append(year.group(1))
-    rawListNames = re.findall(r'<tr align="right"><td>(\d+)</td><td>([A-Za-z]+)', text)
+    rawListNames = re.findall(r'<tr align="right"><td>(\d+)</td><td>(\w+)', text)
 
     sortedListNames = list()
     for element in rawListNames:
@@ -58,10 +72,14 @@ def main():
 
     if args[0] == '--summaryfile':
         del args[0]
-
-    for arg in args:
-        text = '\n'.join(extract_names(arg)) + '\n'
-        print(text)
+        for arg in args:
+            text = '\n'.join(extract_names(arg)) + '\n'
+            with open(arg + '.summary', 'w') as outputFile:
+                outputFile.write(text)
+    else:
+        for arg in args:
+            text = '\n'.join(extract_names(arg)) + '\n'
+            print(text)
 
 
 if __name__ == '__main__':
---
+++ CopySpecial/copyspecial.py
@@ -0,0 +1,40 @@
+import os
+"""Copy Special exercise
+"""
+
+def get_special_paths(dir):
+
+
+def main():
+  # This basic command line argument parsing code is provided.
+  # Add code to call your functions below.
+
+  # Make a list of command line arguments, omitting the [0] element
+  # which is the script itself.
+  args = sys.argv[1:]
+  if not args:
+    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+    sys.exit(1)
+
+  # todir and tozip are either set from command line
+  # or left as the empty string.
+  # The args array is left just containing the dirs.
+  todir = ''
+  if args[0] == '--todir':
+    todir = args[1]
+    del args[0:2]
+
+  tozip = ''
+  if args[0] == '--tozip':
+    tozip = args[1]
+    del args[0:2]
+
+  if len(args) == 0:
+    print "error: must specify one or more dirs"
+    sys.exit(1)
+
+  # +++your code here+++
+  # Call your functions
+  
+if __name__ == "__main__":
+  main()
