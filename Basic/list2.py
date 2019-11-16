50c3dbb1b1c6af14d25eb665407efb4beed64419
ilya <stemasoff1@yandex.ru>
2019-11-16 21:32:33 +0300

    Сделал list2

---
+++ Basic/list2.py
@@ -0,0 +1,45 @@
+# Additional basic list exercises
+
+# D. Given a list of numbers, return a list where
+# all adjacent == elements have been reduced to a single element,
+# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
+# modify the passed in list.
+def remove_adjacent(nums):
+    return list(set(nums))
+
+
+# E. Given two lists sorted in increasing order, create and return a merged
+# list of all the elements in sorted order. You may modify the passed in lists.
+# Ideally, the solution should work in "linear" time, making a single
+# pass of both lists.
+def linear_merge(list1, list2):
+    list1.extend(list2)
+    return sorted(list1)
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = ' X '
+
+    print(f'{prefix} got: {got} expected: {expected}')
+
+
+def main():
+    print('Remove adjacent')
+    test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+    test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+    test(remove_adjacent([]), [])
+
+    print('Linear merge')
+    test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+         ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+
+if __name__ == '__main__':
+    main()
